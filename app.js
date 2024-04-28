f5ff95d383b67a3b7fdf1e4718c023e5f0be6258
Nofisat Hassan <hassannofisat017@gmail.com>
2024-04-28 20:56:41 +0100

    added logger

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const authorsRoute = require("./routes/authors");
+const logger = require("./logger")
 
 const PORT = 5000;
 const app = express();
 
 app.use(bodyParser.json());
 app.use("/authors", authorsRoute);
+app.use(logger)
 
 app.get("/", (req, res) => {
-  res.end("Heyyy, Welcome to the fourth NodeJS assignment");
+  res.send("Hello, Welcome to the fourth NodeJS assignment.");
 });
 
 app.listen(PORT, () => {
